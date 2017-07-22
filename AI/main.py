4c129239d7e6257d5fe4dfed65e54f064b470426
KobusMarais <K.marais42@gmail.com>
2017-07-22 14:48:56 +0200

    no message

--- AI/main.py
+++ AI/main.py
@@ -1,6 +1,6 @@
 ## compute_input.py
 
-import sys, json, numpy as np
+import sys, json as np
 
 #Read data from stdin
 def read_in():
@@ -9,17 +9,8 @@ def read_in():
     return json.loads(lines[0])
 
 def main():
-    #get our data as an array from read_in()
-    lines = read_in()
 
-    #create a numpy array
-    np_lines = np.array(lines)
-
-    #use numpys sum method to find sum of all elements in the array
-    lines_sum = np.sum(np_lines)
-
-    #return the sum to the output stream
-    print(lines_sum)
+    print("Hello world")
 
 #start process
 if __name__ == '__main__':
