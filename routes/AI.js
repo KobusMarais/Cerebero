a468821b85250fcd9ef02949451cea81f7a379a4
KobusMarais <K.marais42@gmail.com>
2017-07-22 12:55:22 +0200

    Added Basic .py script

---
+++ routes/AI.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Kobus on 2017-07-22.
+ */
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+    var spawn = require('child_process').spawn,
+        py    = spawn('python', ['main.py']),
+        data = [1,2,3,4,5,6,7,8,9],
+        dataString = '';
+
+    py.stdout.on('data', function(data){
+        dataString += data.toString();
+    });
+    py.stdout.on('end', function(){
+        console.log('Sum of numbers=',dataString);
+    });
+    py.stdin.write(JSON.stringify(data));
+    py.stdin.end();
+});
+
+module.exports = router;
