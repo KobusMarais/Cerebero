25779ad8d9483e850558dcd2190204e63436ad36
KobusMarais <K.marais42@gmail.com>
2017-07-26 21:21:53 +0200

    Added first API test call

---
+++ routes/apitest.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var text = '{ "success" : 1}';
+    var obj = JSON.parse(text);
+    res.send(obj);
+});
+
+module.exports = router;
