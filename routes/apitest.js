be9ffba0638fb18e0245ad379095b0e3e9dedcb5
KobusMarais <K.marais42@gmail.com>
2017-07-26 21:44:04 +0200

    Fixed syntax error

--- routes/apitest.js
+++ routes/apitest.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
     var text = '"success" : "1"';
     var obj = JSON.parse(text);
     res.send(obj);
