8771af0478fe3509713b5d32f8e6188a4a65e2cc
KobusMarais <K.marais42@gmail.com>
2017-07-26 21:56:33 +0200

    fixed JSON string syntax errors

--- routes/apitest.js
+++ routes/apitest.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var text = '"success" : "1"';
+    var text = '{"success" : "1"}';
     var obj = JSON.parse(text);
     res.send(obj);
 });
