f0781ed3ad5d4a1e8c94f57c2c41ff1856b54437
KobusMarais <K.marais42@gmail.com>
2017-07-26 21:37:27 +0200

    updated JSON testing data

--- routes/apitest.js
+++ routes/apitest.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var text = '{ "success" : 1}';
+router.('/', function(req, res, next) {
+    var text = '"success" : 1';
     var obj = JSON.parse(text);
     res.send(obj);
 });
