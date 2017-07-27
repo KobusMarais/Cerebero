2b6e2b38b279a5b7edeab8f2de68114e842b234c
RikardSchouwstra <rjschouwstra11@gmail.com>
2017-07-27 15:44:36 +0200

    Updated unity project

---
+++ Client/eCivix Election Simulator/Assets/Scripts/GamePlayButtons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GamePlayButtons : MonoBehaviour {
+
+    public Button collectFundsButton;
+    public Button pollProvinceButton;
+    public Button campaignButton;
+
+    // Use this for initialization
+    void Start () {
+
+        Button btn1 = collectFundsButton.GetComponent<Button>();
+        btn1.onClick.AddListener(collectFunds);
+
+        Button btn2 = pollProvinceButton.GetComponent<Button>();
+        btn2.onClick.AddListener(pollProvince);
+
+        Button btn3 = campaignButton.GetComponent<Button>();
+        btn3.onClick.AddListener(campaign);
+    }
+
+    void collectFunds()
+    {
+        print("You have clicked on the collect funds button");
+    }
+
+    void pollProvince()
+    {
+        print("You have clicked on the poll province button");
+    }
+
+    void campaign()
+    {
+        print("You have clicked on the campaign button");
+    }
+}
---
+++ Client/eCivix Election Simulator/Assets/Scripts/NewGameDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGameDetails : MonoBehaviour {
+
+    public Button continueButton;
+
+	// Use this for initialization
+	void Start () {
+
+        Button btn = continueButton.GetComponent<Button>();
+        btn.onClick.AddListener(StartGame);
+    }
+	
+	void StartGame()
+    {
+        SceneManager.LoadScene("MainScreen");
+    }
+}
