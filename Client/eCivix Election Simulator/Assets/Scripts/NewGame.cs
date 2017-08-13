095a01973de67a5d5be0557211ef6ad7d1abef2f
KobusMarais <K.marais42@gmail.com>
2017-08-13 10:29:10 +0200

    Added initialise game

--- Client/eCivix Election Simulator/Assets/Scripts/NewGame.cs
+++ Client/eCivix Election Simulator/Assets/Scripts/NewGame.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NewGame : MonoBehaviour {
-
+  
     void Start()
     {
         Button btn = GetComponent<Button>();
@@ -17,20 +17,5 @@ public class NewGame : MonoBehaviour {
         SceneManager.LoadScene("NewGameScreen");
     }
 
-    void fundmid()
-    {
-        collectFunds();
-        string url = "https://ecivix-testing.herokuapp.com/api/getFunds";
-        www = new WWW(url);
-        StartCoroutine(collectFunds());
-    }
-
-    IEnumerator collectFunds()
-    {
-        print("You have clicked on the collect funds button");
-
-        yield return www;
-        print(www.text);
-
-    }
+  
 }
