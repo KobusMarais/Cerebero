fb0fbf1a282cb1c7de2e3bc594cefb377cc72488
KobusMarais <K.marais42@gmail.com>
2017-08-14 12:13:51 +0200

    Fixed underconstruction page

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var path = require('path');
-    res.sendFile(path.resolve('./Client/eCivix Election Simulator/Builds/index.html'));
+    res.sendFile(path.resolve('./Client/eCivix Election Simulator/UnderConstructionPage/index.html'));
 
 });
 
