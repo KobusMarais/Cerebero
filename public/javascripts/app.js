f3369ca0cb0d83352948774abecde7eef42900a3
FredJEhlers <fehlers@retrorabbit.co.za>
2017-08-22 06:10:46 +0200

    Seperate login page and profile page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,6 @@
 // Get users
 angular.module('nodeUser', [])
-.controller('mainController', ($scope, $http) => {
+.controller('mainController', ($scope, $location, $http) => {
     $scope.formData = {};
     $scope.userData = {};
     // Get all users
@@ -37,3 +37,62 @@ angular.module('nodeUser', [])
         });
     };
 });
+
+ // User profiles 
+ angular.module('nodeProfile', [])
+ .controller('mainController', ($scope, $http) => {
+     $scope.formData = {};
+     $scope.userData = {};
+    
+    $scope.slider = {
+        value: 100,
+        options: {
+            showSelectionBar: true
+        }
+    };
+
+    // Insert user profile into the DB 
+    $scope.setUserProfile = () => {
+        $http.post('/api/v1/createProfile', $scope.formData)
+        .success((data) => {
+            $scope.formData = {};
+            $scope.userData = data;
+            console.log(data);
+        })
+        .error((error) => {
+            console.log('Error: ' + error);
+        });
+    };
+});
+
+ // User profiles 
+ angular.module('nodeLogin', [])
+ .controller('mainController', ($scope, $http) => {
+     $scope.formData = {};
+     $scope.userData = {};
+    
+    $scope.slider = {
+        value: 100,
+        options: {
+            showSelectionBar: true
+        }
+    };
+
+    // Login a registered user
+    $scope.loginUser = () => {
+        $http.post('/api/v1/loginUsers', $scope.formData)
+        .success((data) => {
+            $scope.formData = {};
+            $scope.userData = data;
+            console.log(data);
+            $window.$location.$http  = '/profile.html'
+            // router.get('/', (req, res, next) => {
+            //     res.sendFile(path.join(
+            //       __dirname, '..', 'public', 'index.html'));
+            //   });
+        })
+        .error((error) => {
+            console.log('Error: ' + error);
+        });
+    };
+});  
