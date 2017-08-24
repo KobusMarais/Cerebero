09282ad4db12c32cb3bd4ad58d3c0f004fac0354
KobusMarais <K.marais42@gmail.com>
2017-08-24 19:23:28 +0200

    fixed first page load back to under construction page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://testUser:testTodo@localhost:5432/todo';
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/loginpage', (req, res, next) => {
   res.sendFile(path.join(
     __dirname, '..', '..', 'client', 'views', 'index.html'));
 });
