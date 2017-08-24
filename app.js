9830288ad7bb91ec16f0ed61f6d657cc096fd22e
KobusMarais <K.marais42@gmail.com>
2017-08-24 19:50:36 +0200

    only removed code that broke stuff

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-//var logger = require('morgan');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -16,23 +16,23 @@ var path = require('path');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/index', index);
+app.use('/index', index);
 
 app.use('/AI', AI);
 
 app.use('/', users);
-console.log("THIS IS WHERE THE DOG SLEEPS 5");
+
 app.use('/api', api);
 app.use('/mainGame', mainGame)
 app.use(express.static(__dirname + '/Client/eCivix Election Simulator/Builds/'));
