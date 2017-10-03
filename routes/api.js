029ac8d5a5c1838a83b7bdaf6f60fca812cc3d8b
KobusMarais <K.marais42@gmail.com>
2017-10-03 08:34:47 +0200

    added else statement to ensure no double returns

--- routes/api.js
+++ routes/api.js
@@ -19,13 +19,15 @@ router.post('/register', function(req,res){
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    let i = req.body;
-    //generate new token for each user and create entry in db for that user.
-    queries.register(i.name, i.surname, i.email, i.username, i.password, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+    else {
+        let i = req.body;
+        //generate new token for each user and create entry in db for that user.
+        queries.register(i.name, i.surname, i.email, i.username, i.password, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
     //Insert code here to check if email or username has been used before
 
 });
@@ -47,12 +49,14 @@ router.post('/login', function(req,res){
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    let i = req.body;
-    queries.login(i.username, i.password, function(err, result) {
-        if (err) return console.log("error: ", err);
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+    else {
+        let i = req.body;
+        queries.login(i.username, i.password, function (err, result) {
+            if (err) return console.log("error: ", err);
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 });
 
 router.post('/collectFunds', function(req,res){
@@ -73,15 +77,17 @@ router.post('/collectFunds', function(req,res){
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    /// /find access token in DB
-    //calculate fund change according to dataset from db
-    //save amount of funds user has to db.
-    let i = req.body;
-    queries.collectFunds(i.access_token, i.province, function(err, result) {
-        if (err) return console.log("error: ", err);
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+    else {
+        /// /find access token in DB
+        //calculate fund change according to dataset from db
+        //save amount of funds user has to db.
+        let i = req.body;
+        queries.collectFunds(i.access_token, i.province, function (err, result) {
+            if (err) return console.log("error: ", err);
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
     //return success and update funds
 });
 router.post('/getTopic', function(req,res) {
@@ -101,12 +107,14 @@ router.post('/getTopic', function(req,res) {
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    let i = req.body;
-    queries.getTopic(i.access_token, function(err, result) {
-        if (err) return console.log("error: ", err);
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+    else {
+        let i = req.body;
+        queries.getTopic(i.access_token, function (err, result) {
+            if (err) return console.log("error: ", err);
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 });
 
 router.post('/campaignProvince', function(req,res){
@@ -127,19 +135,21 @@ router.post('/campaignProvince', function(req,res){
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    //find access token in DB
-    //calculate support change according to dataset from db
-    //save amount of funds user has to db.
-    let i = req.body;
-   /* queries.campaignProvince(i.access_token, i.province, i.topic, function(err, result) {
-        if (err) return console.log("error: ", err);
-        var obj = JSON.parse(result);
+    else {
+        //find access token in DB
+        //calculate support change according to dataset from db
+        //save amount of funds user has to db.
+        let i = req.body;
+        /* queries.campaignProvince(i.access_token, i.province, i.topic, function(err, result) {
+             if (err) return console.log("error: ", err);
+             var obj = JSON.parse(result);
+             res.send(obj);
+         });*/
+        //return success and update funds
+        var text = '{"success" : "1", "support" : "3000", "AI1Move" : "Campaign Western Cape", "AI2Move" : "Collect Funds Freestate" , "AI3Move" : "Poll Limpopo", "AI4Move" : "Poll Gauteng"}';
+        var obj = JSON.parse(text);
         res.send(obj);
-    });*/
-    //return success and update funds
-    var text = '{"success" : "1", "support" : "3000", "AI1Move" : "Campaign Western Cape", "AI2Move" : "Collect Funds Freestate" , "AI3Move" : "Poll Limpopo", "AI4Move" : "Poll Gauteng"}';
-    var obj = JSON.parse(text);
-    res.send(obj);
+    }
 });
 
 router.post('/pollProvince', function(req,res){
@@ -159,14 +169,15 @@ router.post('/pollProvince', function(req,res){
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
-    }
-    //find access token in DB
-    //retrieve opponent's in that province from db
-    //return support to user
+    } else {
+        //find access token in DB
+        //retrieve opponent's in that province from db
+        //return support to user
 
-    var text = '{"User": "40", "AI1" : "10", "AI2" : "10", "AI3": "20", "AI4" : "20", "AI1Move" : "Poll Gauteng", "AI2Move" : "Poll Limpopo" , "AI3Move" : "Collect Funds Freestate", "AI4Move" : "Campaign Western Cape"}';
-    var obj = JSON.parse(text);
-    res.send(obj);
+        var text = '{"User": "40", "AI1" : "10", "AI2" : "10", "AI3": "20", "AI4" : "20", "AI1Move" : "Poll Gauteng", "AI2Move" : "Poll Limpopo" , "AI3Move" : "Collect Funds Freestate", "AI4Move" : "Campaign Western Cape"}';
+        var obj = JSON.parse(text);
+        res.send(obj);
+    }
 });
 
 router.post('/getFunds', function(req, res, next) { //this is a national overall value
@@ -187,12 +198,13 @@ router.post('/getFunds', function(req, res, next) { //this is a national overall
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
+    } else {
+        queries.getFunds(req.body.access_token, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
     }
-    queries.getFunds(req.body.access_token, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
 });
 
 router.post('/getFundsProvince', function(req, res, next) { //this is how many funds available per province
@@ -213,13 +225,14 @@ router.post('/getFundsProvince', function(req, res, next) { //this is how many f
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
-    }
+    } else {
 
-    queries.getFundsProvince(req.body.access_token, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+        queries.getFundsProvince(req.body.access_token, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 
 });
 
@@ -241,12 +254,14 @@ router.post('/getProfile', function(req, res, next) {
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    //return user data
-    queries.getProfile(req.body.access_token, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+    else {
+        //return user data
+        queries.getProfile(req.body.access_token, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 });
 
 router.post('/getScore', function(req, res, next) {
@@ -266,13 +281,14 @@ router.post('/getScore', function(req, res, next) {
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
-    }
+    } else {
     //return score
     queries.getScore(req.body.access_token, function(err, result) {
         if (err) return console.log("error: ", err)
         var obj = JSON.parse(result);
         res.send(obj);
     });
+    }
 });
 
 router.post('/getManpower', function(req, res, next) { //this is an overall national total
@@ -293,13 +309,14 @@ router.post('/getManpower', function(req, res, next) { //this is an overall nati
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
+    }  else {
+        //return manpower user has currently
+        queries.getManpower(req.body.access_token, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
     }
-    //return manpower user has currently
-    queries.getManpower(req.body.access_token, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
 });
 
 router.post('/getSupport', function(req, res, next) { // each province has its own support
@@ -320,13 +337,14 @@ router.post('/getSupport', function(req, res, next) { // each province has its o
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
-    }
+    } else {
 
-    queries.getSupport(req.body.access_token, req.body.province, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+        queries.getSupport(req.body.access_token, req.body.province, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 });
 
 router.get('/getHighscoreBoard', function(req, res, next) {
@@ -361,14 +379,15 @@ router.post('/startGame', function(req, res, next) { // initialises all values a
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
-    }
+    }  else {
 
-    //return everything that needs to be displayed on client side
-    queries.startGame(req.body.access_token, function(err, result) {
-        if (err) return console.log("error: ", err);
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+        //return everything that needs to be displayed on client side
+        queries.startGame(req.body.access_token, function (err, result) {
+            if (err) return console.log("error: ", err);
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 });
 
 
@@ -391,13 +410,16 @@ router.post('/setAI', function(req, res, next) { // initialises all values at th
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    //find access token in DB
-    //create and retrieve all starter info for user
+    else {
+        //find access token in DB
+        //create and retrieve all starter info for user
 
-    //return everything that needs to be displayed on client side
-    var text = '{"Username" : "Jack", "Funds" : "0", "TotalSupport" : "0", "Manpower": "0", "Weeks" : "3"}';
-    var obj = JSON.parse(text);
-    res.send(obj);
+        //return everything that needs to be displayed on client side
+        var text = '{"Username" : "Jack", "Funds" : "0", "TotalSupport" : "0", "Manpower": "0", "Weeks" : "3"}';
+        var obj = JSON.parse(text);
+        res.send(obj);
+
+    }
 });
 
 router.post('/endTurn', function(req, res, next) { // AIs make their final move and date increased.
@@ -415,16 +437,17 @@ router.post('/endTurn', function(req, res, next) { // AIs make their final move 
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
-    }
-    //find access token in DB
-    //run AI
-    //Decrease time before election and returns it
+    } else {
+        //find access token in DB
+        //run AI
+        //Decrease time before election and returns it
 
-    queries.endTurn(req.body.access_token, function(err, result) {
-        if (err) return console.log("error: ", err);
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+        queries.endTurn(req.body.access_token, function (err, result) {
+            if (err) return console.log("error: ", err);
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 });
 
 router.post('/endHighScore', function(req, res, next) { // receives user score and inserts into leaderboard
@@ -442,11 +465,13 @@ router.post('/endHighScore', function(req, res, next) { // receives user score a
         var obj = JSON.parse(result);
         res.send(obj);
     }
-    queries.endHighScore(req.body.access_token, req.body.userScore, function(err, result) {
-        if (err) return console.log("error: ", err);
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
+    else {
+        queries.endHighScore(req.body.access_token, req.body.userScore, function (err, result) {
+            if (err) return console.log("error: ", err);
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
+    }
 });
 
 router.get('/getIssues', function(req, res, next) {
@@ -479,14 +504,15 @@ router.post('/getStances', function(req, res, next) { // receives user score and
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
+    }  else {
+        console.log(req.body.issues.length);
+        let i = req.body.issues; // for brevity
+        queries.getStances(i, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
     }
-    console.log(req.body.issues.length);
-    let i = req.body.issues; // for brevity
-    queries.getStances(i, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
 });
 
 router.post('/setIssues', function(req, res, next) { // receives user score and inserts into leaderboard
@@ -504,13 +530,14 @@ router.post('/setIssues', function(req, res, next) { // receives user score and 
         var result = JSON.stringify(obj);
         var obj = JSON.parse(result);
         res.send(obj);
+    }  else {
+        let i = req.body; // for brevity
+        queries.setIssues(i.access_token, i.issues, function (err, result) {
+            if (err) return console.log("error: ", err)
+            var obj = JSON.parse(result);
+            res.send(obj);
+        });
     }
-    let i = req.body; // for brevity
-    queries.setIssues(i.access_token, i.issues, function(err, result) {
-        if (err) return console.log("error: ", err)
-        var obj = JSON.parse(result);
-        res.send(obj);
-    });
 });
 //query.on('end', () => { client.end(); });
 module.exports = router;
