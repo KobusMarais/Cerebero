97ee7441e556a4b0ad0cef4ff30cdcf61e83cd28
FredJEhlers <fehlers@retrorabbit.co.za>
2017-10-08 19:57:16 +0200

    removed no longer needed require

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
-const index = require('./routes/index');
 const users = require('./routes/users');
 const AI = require('./routes/AI');
 const api = require('./routes/api');
@@ -25,8 +23,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/index', index);
 
 app.use('/AI', AI);
 
