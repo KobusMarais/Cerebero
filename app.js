063c050e44ffd7a045b0ca8f5fcf86fda49e0b9d
FredJEhlers <fehlers@retrorabbit.co.za>
2017-10-09 07:33:56 +0200

    Made the login page the default start page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+
 const users = require('./routes/users');
 const AI = require('./routes/AI');
 const api = require('./routes/api');
@@ -12,9 +13,9 @@ const databasers = require('./routes/database.js');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,23 +23,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/AI', AI);
 
-app.use('/', users);
-
-app.use('/api', api);
+app.use('/', api);
 app.use('/mainGame', mainGame);
 app.use('/dbtest', databasers);
-app.use(express.static(__dirname + '/Client/eCivix Election Simulator/Builds/'));
+app.use(express.static(__dirname + '/Client/eCivix Election Simulator/public/'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 
 // error handler
 app.use(function(err, req, res, next) {
