afbd5a4c6df1cf91c134652e92df31984c8ec7e0
VictorMac <vtwigge@yahoo.com>
2017-10-10 08:01:33 +0200

    Added Unity access token function

--- Client/eCivix Election Simulator/Assets/Scripts/NewGame.cs
+++ Client/eCivix Election Simulator/Assets/Scripts/NewGame.cs
@@ -17,14 +17,20 @@ public class NewGame : MonoBehaviour {
 		Button btn = GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
 
-		//access_token = getAccessToken();
-
-		access_token = "2";
-
-		/*
-		if (access_token == null)
+		try   
+		{  
+			access_token = getAccessToken(); 
+		}  
+		catch  
+		{
 			access_token = "2";
-		*/
+		}
+
+
+
+		//access_token = "2";
+		//if (access_token == null)
+		//	access_token = "2";
 	}
 
 	void TaskOnClick()
